70c22362c785efe9db26993de28df459ac1fe17c
Legiew <sebweigel@gmail.com>
2016-02-01 11:33:02 +0100

    Add first bridge structure

--- src/bridge/BridgeStructure.java
+++ src/bridge/BridgeStructure.java
@@ -11,6 +11,19 @@ public class BridgeStructure
 {
 	private Model model;
 
+	private final int bottom = 0;
+	private final int middle = 1000000;
+	private final int top = 2000000;
+	private final int front = 0;
+	private final int back = 100000;
+	private final int crosswise = 10000;
+	private final int angular = 20000;
+
+	private double length = 20.0;
+	private int nodeCountLength = 20;
+	private double width = 5.0;
+	private double height = 3.0;
+
 	public BridgeStructure()
 	{
 		this.model = new Model();
@@ -23,18 +36,109 @@ public class BridgeStructure
 		section.setDiameter(0.15);
 		section.setWTK(0.002);
 
-		double x = 0.0;
-		double dx = 2.0;
+		double nodeLengthDelta = length / (nodeCountLength - 1.0);
 
-		for (int i = 0; i < 6; i++)
+		createBottomGrid(nodeCountLength, width, bottom, front, back, crosswise, material, section, nodeLengthDelta);
+
+		createAngularStart(height, bottom, middle, top, front, angular, material, section, nodeLengthDelta, 0);
+
+		createAngularStart(height, bottom, middle, top, back, angular, material, section, nodeLengthDelta, width);
+
+		createAngularEnd(height, bottom, middle, top, front, angular, material, section, nodeLengthDelta, 0);
+
+		createAngularEnd(height, bottom, middle, top, back, angular, material, section, nodeLengthDelta, width);
+
+		for (int i = 3; i < nodeCountLength - 4; i++)
 		{
-			this.model.createNode(i + 1, x, i % 2 + 1, i % 2);
-			x += dx;
+			this.model.createNode(top + front + i, nodeLengthDelta * i, height, 0);
+			this.model.createNode(top + back + i, nodeLengthDelta * i, height, width);
+			this.model.createNode(middle + front + i, nodeLengthDelta * i, height / 2.0, 0);
+			this.model.createNode(middle + back + i, nodeLengthDelta * i, height / 2.0, width);
 		}
 
-		for (int i = 0; i < 5; i++)
+		for (int i = 2; i < nodeCountLength - 3; i++)
 		{
-			model.createElement(i + 1, Truss3D.TYPE, material, section, model.getNode(i + 1), model.getNode(i + 2));
+			model.createElement(top + front + i, Truss3D.TYPE, material, section, model.getNode(top + front + i),
+					model.getNode(top + front + i + 1));
+
+			model.createElement(top + back + i, Truss3D.TYPE, material, section, model.getNode(top + back + i),
+					model.getNode(top + back + i + 1));
+		}
+	}
+
+	private void createAngularStart(double height, final int bottom, final int middle, final int top, final int zLayer,
+			final int angular, Material material, HollowCircleS section, double nodeLengthDelta, double width)
+	{
+		this.model.createNode(middle + zLayer + 1, nodeLengthDelta, height / 2.0, width);
+
+		this.model.createNode(top + zLayer + 2, nodeLengthDelta * 2.0, height, width);
+
+		model.createElement(bottom + zLayer + angular, Truss3D.TYPE, material, section, model.getNode(bottom + zLayer),
+				model.getNode(middle + zLayer + 1));
+
+		model.createElement(bottom + zLayer + angular + 1, Truss3D.TYPE, material, section,
+				model.getNode(middle + zLayer + 1), model.getNode(top + zLayer + 2));
+
+		model.createElement(bottom + zLayer + angular + 2, Truss3D.TYPE, material, section,
+				model.getNode(bottom + zLayer + 1), model.getNode(middle + zLayer + 1));
+
+		model.createElement(bottom + zLayer + angular + 3, Truss3D.TYPE, material, section,
+				model.getNode(top + zLayer + 2), model.getNode(bottom + zLayer + 2));
+
+		model.createElement(bottom + zLayer + angular + 4, Truss3D.TYPE, material, section,
+				model.getNode(middle + zLayer + 1), model.getNode(bottom + zLayer + 2));
+	}
+
+	private void createAngularEnd(double height, final int bottom, final int middle, final int top, final int zLayer,
+			final int angular, Material material, HollowCircleS section, double nodeLengthDelta, double width)
+	{
+		int firstNode = middle + zLayer + this.nodeCountLength - 2;
+		int secondNode = top + zLayer + this.nodeCountLength - 3;
+
+		int endIndex = (this.nodeCountLength - 4) * 6;
+
+		this.model.createNode(firstNode, nodeLengthDelta * (this.nodeCountLength - 2), height / 2.0, width);
+
+		this.model.createNode(secondNode, nodeLengthDelta * (this.nodeCountLength - 3), height, width);
+
+		model.createElement(bottom + zLayer + angular + endIndex, Truss3D.TYPE, material, section,
+				model.getNode(bottom + zLayer + this.nodeCountLength - 1), model.getNode(firstNode));
+
+		model.createElement(bottom + zLayer + angular + endIndex + 1, Truss3D.TYPE, material, section,
+				model.getNode(firstNode), model.getNode(secondNode));
+
+		model.createElement(bottom + zLayer + angular + endIndex + 2, Truss3D.TYPE, material, section,
+				model.getNode(bottom + zLayer + this.nodeCountLength - 2), model.getNode(firstNode));
+
+		model.createElement(bottom + zLayer + angular + endIndex + 3, Truss3D.TYPE, material, section,
+				model.getNode(secondNode), model.getNode(bottom + zLayer + this.nodeCountLength - 3));
+
+		model.createElement(bottom + zLayer + angular + endIndex + 4, Truss3D.TYPE, material, section,
+				model.getNode(firstNode), model.getNode(bottom + zLayer + this.nodeCountLength - 3));
+	}
+
+	private void createBottomGrid(int nodeCountLength, double width, final int bottom, final int front, final int back,
+			final int crosswise, Material material, HollowCircleS section, double nodeLengthDelta)
+	{
+		for (int i = 0; i < nodeCountLength; i++)
+		{
+			this.model.createNode(bottom + front + i, nodeLengthDelta * i, 0, 0);
+			this.model.createNode(bottom + back + i, nodeLengthDelta * i, 0, width);
+		}
+
+		for (int i = 0; i < nodeCountLength - 1; i++)
+		{
+			model.createElement(bottom + front + i, Truss3D.TYPE, material, section, model.getNode(bottom + front + i),
+					model.getNode(bottom + front + i + 1));
+
+			model.createElement(bottom + back + i, Truss3D.TYPE, material, section, model.getNode(bottom + back + i),
+					model.getNode(bottom + back + i + 1));
+		}
+
+		for (int i = 0; i < nodeCountLength; i++)
+		{
+			model.createElement(bottom + crosswise + i, Truss3D.TYPE, material, section,
+					model.getNode(bottom + front + i), model.getNode(bottom + back + i));
 		}
 	}
 
